2e5613f7c1da20d374aca3ac67f056cb5647cb1b
tanatuosi <49973816+tanatuosi@users.noreply.github.com>
2023-08-29 02:42:19 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -18,5 +18,68 @@ export function formate(
   formatter: string | ((dateInfo: DateInfo) => string),
   isPad = false
 ): string {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+  const yyyy = String(year);
+  const MM = String(month);
+  const dd = String(day);
+  const hh = String(hour);
+  const mm = String(minute);
+  const ss = String(second);
+  const padMonth = month < 10 ? `0${month}` : MM;
+  const padDay = day < 10 ? `0${day}` : dd;
+  const padHour = hour < 10 ? `0${hour}` : hh;
+  const padMinute = minute < 10 ? `0${minute}` : mm;
+  const padSecond = second < 10 ? `0${second}` : ss;
+  if (typeof formatter === "string") {
+    if (isPad) {
+      if (formatter === "date") {
+        return `${year}-${padMonth}-${day}`;
+      }
+      if (formatter === "datetime") {
+        return `${year}-${padMonth}-${padDay} ${padHour}:${padMinute}:${padSecond}`;
+      }
+      const timeOut = formatter
+        .replaceAll("yyyy", yyyy)
+        .replaceAll("MM", padMonth)
+        .replaceAll("hh", padHour)
+        .replaceAll("mm", padMinute)
+        .replaceAll("ss", padSecond)
+        .replaceAll("dd", padDay);
+      return timeOut;
+    }
+    if (formatter === "date") return `${year}-${month}-${day}`;
+    if (formatter === "datetime") {
+      return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+    }
+    const timeOut = formatter
+      .replaceAll("yyyy", yyyy)
+      .replaceAll("MM", MM)
+      .replaceAll("hh", hh)
+      .replaceAll("mm", mm)
+      .replaceAll("ss", ss)
+      .replaceAll("dd", dd);
+    return timeOut;
+  }
+  if (typeof formatter === "function") {
+    return formatter({
+      year,
+      month,
+      day,
+      hour,
+      minute,
+      second,
+      yyyy,
+      MM,
+      dd,
+      hh,
+      mm,
+      ss,
+    });
+  }
   return "";
 }
